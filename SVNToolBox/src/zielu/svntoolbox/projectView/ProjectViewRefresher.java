483813b39ffcd78a9715bd07b12f0794a46f1c3d
Lukasz Zielinski <zieluuuu@gmail.com>
2013-09-23 18:56:40 +0200

    + extracted 'Configure branches...' as context action

--- SVNToolBox/src/zielu/svntoolbox/projectView/ProjectViewRefresher.java
+++ SVNToolBox/src/zielu/svntoolbox/projectView/ProjectViewRefresher.java
@@ -6,6 +6,9 @@ package zielu.svntoolbox.projectView;
 import com.intellij.ide.projectView.ProjectView;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.changes.committed.VcsConfigurationChangeListener;
+import com.intellij.openapi.vcs.changes.committed.VcsConfigurationChangeListener.Notification;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.messages.MessageBusConnection;
 
 /**
@@ -22,14 +25,24 @@ public class ProjectViewRefresher extends AbstractProjectComponent {
         super(project);
     }
 
+    private void refreshProjectView(Project project) {
+        ProjectView projectView = ProjectView.getInstance(project);
+        projectView.refresh();
+    }
+    
     @Override
     public void projectOpened() {
-        MessageBusConnection connection = myProject.getMessageBus().connect();
+        connection = myProject.getMessageBus().connect();
         connection.subscribe(DecorationToggleNotifier.TOGGLE_TOPIC, new DecorationToggleNotifier() {
             @Override
             public void decorationChanged() {
-                ProjectView projectView = ProjectView.getInstance(myProject);
-                projectView.refresh();
+                refreshProjectView(myProject);
+            }
+        });
+        connection.subscribe(VcsConfigurationChangeListener.BRANCHES_CHANGED, new Notification() {
+            @Override
+            public void execute(Project project, VirtualFile vcsRoot) {
+                refreshProjectView(project);
             }
         });
     }
---
+++ SVNToolBox/src/zielu/svntoolbox/ui/actions/ConfigureBranchesAction.java
@@ -0,0 +1,61 @@
+/* 
+ * $Id$
+ */
+package zielu.svntoolbox.ui.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.ProjectLevelVcsManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.idea.svn.SvnVcs;
+import org.jetbrains.idea.svn.dialogs.BranchConfigurationDialog;
+
+/**
+ * <p></p>
+ * <br/>
+ * <p>Created on 23.09.13</p>
+ *
+ * @author Lukasz Zielinski
+ */
+public class ConfigureBranchesAction extends AnAction {
+
+
+    @Override
+    public void update(AnActionEvent e) {
+        super.update(e);
+        Presentation presentation = e.getPresentation();
+        final DataContext dataContext = e.getDataContext();
+
+        Project project = PlatformDataKeys.PROJECT.getData(dataContext);
+        if (project == null) {
+            presentation.setEnabled(false);
+            presentation.setVisible(false);
+            return;
+        }
+        VirtualFile vFile = PlatformDataKeys.VIRTUAL_FILE.getData(dataContext);
+        if (vFile == null) {
+            presentation.setEnabled(false);
+            presentation.setVisible(true);
+            return;
+        }
+        SvnVcs vcs = SvnVcs.getInstance(project);
+        if (!ProjectLevelVcsManager.getInstance(project).checkAllFilesAreUnder(vcs, new VirtualFile[]{vFile})) {
+            presentation.setEnabled(false);
+            presentation.setVisible(true);
+            return;
+        }
+        presentation.setEnabled(true);
+        presentation.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        Project project = e.getProject();
+        VirtualFile vFile = PlatformDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+        BranchConfigurationDialog.configureBranches(project, vFile);
+    }
+}
