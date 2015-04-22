55268a821a8c531e51d720fb4a6bdbaff2b44b0d
Abono <bonoadil@gmail.com>
2015-04-22 15:59:56 +0200

    ABO add svn info popup

---
+++ SVNToolBox/src/zielu/svntoolbox/ui/actions/ShowLockInfoAction.java
@@ -0,0 +1,93 @@
+package zielu.svntoolbox.ui.actions;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ide.CopyPasteManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.JBPopupFactory;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.components.JBList;
+import com.jgoodies.common.base.Strings;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.idea.svn.SvnVcs;
+import org.jetbrains.idea.svn.commandLine.SvnBindException;
+import org.jetbrains.idea.svn.info.Info;
+import org.jetbrains.idea.svn.lock.Lock;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+
+import javax.swing.*;
+import java.awt.datatransfer.StringSelection;
+import java.util.List;
+
+import static com.google.common.collect.Iterables.getLast;
+import static org.apache.commons.lang.StringUtils.EMPTY;
+import static zielu.svntoolbox.SvnToolBoxBundle.getString;
+
+/**
+ * <p></p>
+ * <br/>
+ * <p>Created on 22.04.15</p>
+ *
+ * @author BONO Adil
+ */
+public class ShowLockInfoAction extends VirtualFileUnderSvnActionBase {
+
+    public ShowLockInfoAction() {
+        super(getString("action.show.lock.info"));
+    }
+
+    private static final String FIELD_DELIMITER = " : ";
+    private static final Splitter splitter = Splitter.on(FIELD_DELIMITER).trimResults();
+
+    @Override
+    protected void perform(AnActionEvent e, @NotNull Project project, @NotNull VirtualFile file) {
+        //get url
+        SvnVcs svn = SvnVcs.getInstance(project);
+        Info fileInfo = svn.getInfo(file);
+        if (fileInfo == null) {
+            return ;
+        }
+
+        //get lock infos
+        try {
+            Info urlInfo = svn.getInfo(fileInfo.getURL(), SVNRevision.UNDEFINED);
+            if(urlInfo == null)
+                return;
+
+            List<String> datas = Lists.newArrayList();
+
+            Lock lock = urlInfo.getLock();
+            if(lock != null) {
+                datas.add(getString("configurable.app.svnlock.owner.label") + FIELD_DELIMITER + lock.getOwner());
+                datas.add(getString("configurable.app.svnlock.comment.label") + FIELD_DELIMITER + lock.getComment());
+                datas.add(getString("configurable.app.svnlock.creation.label") + FIELD_DELIMITER + (lock.getCreationDate() != null ? lock.getCreationDate() : EMPTY));
+                datas.add(getString("configurable.app.svnlock.expiration.label") + FIELD_DELIMITER + (lock.getExpirationDate() != null ? lock.getExpirationDate() : EMPTY));
+            }
+
+            final JList list = new JBList(datas);
+
+            JBPopupFactory.getInstance()
+                .createListPopupBuilder(list)
+                .setTitle(getString("configurable.app.svnlock.title"))
+                .setResizable(true)
+                .setItemChoosenCallback(new Runnable() {
+                        public void run() {
+                            if (list.getSelectedIndices().length > 0) {
+                                String selectedValue = (String) list.getSelectedValue();
+
+                                if (Strings.isNotBlank(selectedValue) && selectedValue.contains(FIELD_DELIMITER)) {
+                                    CopyPasteManager.getInstance().setContents(new StringSelection(getLast(splitter.split(selectedValue))));
+                                }
+                            }
+                        }
+                    }
+                ).createPopup().showInFocusCenter();
+
+
+        } catch (SvnBindException sbe) {
+            sbe.printStackTrace();
+            return;
+        }
+    }
+}
